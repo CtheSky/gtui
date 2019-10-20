5abba2ea07ed797725ac529e38de66bd64261b98
Tianhong Chu <cthesky@yeah.net>
2019-10-20 15:56:46 +0800

    declare install denpendency in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-import os
 from setuptools import setup, find_packages
 
-# read requirement.txt
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'requirements.txt')) as f:
-    requirements = [_.strip() for _ in f.readlines() if not _.startswith('#')]
 
 setup(
-    install_requires=requirements,
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=[
+        'urwid>=2.0.0',
+        'additional-urwid-widgets==0.4.1',
+        'pyperclip>=1.7.0'
+    ]
 )
