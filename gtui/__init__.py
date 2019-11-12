750d2e8a9829b342235056f47e6e46c75d794c37
Tianhong Chu <cthesky@yeah.net>
2019-11-12 17:38:47 +0800

    import callback module in __init__

--- gtui/__init__.py
+++ gtui/__init__.py
@@ -1,5 +1,6 @@
 """Simple Job Scheduler With Friendly Text User Interface"""
 from .task import Task, TaskGraph
 from .executor import IORedirectedThread
+from . import callback
 
 __version__ = '0.1.0'
